604dc8330e06488ca49d62ba320c95c7ffbb9a02
helter daniel <helterdaniel@hotmail.com>
2021-06-10 19:52:13 -0300

    criado topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { NotificationsNone, Language, Settings } from '@material-ui/icons'
+import "./topbar.css"
+
+const Topbar = () => {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">LamaAdmin</span>
+
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+
+                    </div>
+                    <img src="https://randomuser.me/api/portraits/women/68.jpg" alt="avatar" className="topAvatar" />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Topbar
