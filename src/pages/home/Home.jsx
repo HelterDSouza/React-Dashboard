263b399ba1bd38952b62050a9f0aeb77b787424f
helter daniel <helterdaniel@hotmail.com>
2021-06-12 20:24:01 -0300

    fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import "./home.css"
 const Home = () => {
     return (
         <div className="home">
-            home
+            <FeaturedInfo />
         </div>
     )
 }
