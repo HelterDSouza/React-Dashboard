44ecfe0537df27aa8fb97621c9bb157099ea373a
helter daniel <helterdaniel@hotmail.com>
2021-06-12 20:23:44 -0300

    featuredComponent

---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./featuredInfo.css"
+import { ArrowDownward, ArrowUpward } from '@material-ui/icons'
+const FeaturedInfo = () => {
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <span className="featuredTitle">Tevanue</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">R$2,415</span>
+                    <span className="featuredMoneyRate">
+                        -2,415 <ArrowDownward className="featuredIcon negative" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared To las month</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Sales</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">R$2,415</span>
+                    <span className="featuredMoneyRate">
+                        -2,415 <ArrowDownward className="featuredIcon negative" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared To las month</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Cost</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">R$2,415</span>
+                    <span className="featuredMoneyRate">
+                        -2,415 <ArrowUpward className="featuredIcon" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared To las month</span>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedInfo
