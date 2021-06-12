e6106ee382b3e85858eff66a40d76d8b796d6f3a
helter daniel <helterdaniel@hotmail.com>
2021-06-12 17:40:04 -0300

    pagina inicial

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@
 import Sidebar from "./components/sidebar/Sidebar"
 import Topbar from "./components/topbar/Topbar"
 import "./app.css"
+import Home from "./pages/home/Home"
 function App() {
   return (
     <div className="App">
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        Other pages
+        <Home/>
       </div>
     </div>
   );
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./home.css"
+const Home = () => {
+    return (
+        <div className="home">
+            home
+        </div>
+    )
+}
+
+export default Home
