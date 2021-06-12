494084b60c6578c9b0cd1db534c77ec3bb1c3096
helter daniel <helterdaniel@hotmail.com>
2021-06-12 20:41:12 -0300

    chartComponent

---
+++ src/dataExample.js
@@ -0,0 +1,63 @@
+export const userData = [
+        {
+            name: 'Jan',
+            "Active User": 5000,
+
+        },
+        {
+            name: 'Fev',
+            "Active User": 1000,
+
+        },
+        {
+            name: 'Mar',
+            "Active User": 2000,
+
+        },
+        {
+            name: 'Abr',
+            "Active User": 9000,
+
+        },
+        {
+            name: 'Mai',
+            "Active User": 8000,
+
+        },
+        {
+            name: 'Jun',
+            "Active User": 7000,
+
+        },
+        {
+            name: 'Jul',
+            "Active User": 6000,
+
+        },
+        {
+            name: 'Ago',
+            "Active User": 5000,
+
+        },
+        {
+            name: 'Set',
+            "Active User": 4000,
+
+        },
+        {
+            name: 'Out',
+            "Active User": 3000,
+
+        },
+        {
+            name: 'Nov',
+            "Active User": 2000,
+
+        },
+        {
+            name: 'Des',
+            "Active User": 1000,
+
+        },
+
+    ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import Chart from '../../components/chart/Chart'
 import "./home.css"
+import { userData } from '../../dataExample'
 const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
         </div>
     )
 }
