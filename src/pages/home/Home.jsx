c088eccf633c2a907624c869d55eeeda7ba843fb
helter daniel <helterdaniel@hotmail.com>
2021-06-22 19:04:30 -0300

    import widgets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import Chart from '../../components/chart/Chart'
 import "./home.css"
 import { userData } from '../../dataExample'
+import WidgetSmall from '../../components/widgetsSmall/WidgetSmall'
+import WidgetLarge from '../../components/widgetsLarge/WidgetLarge'
 const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo />
             <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+            <div className="homeWidgets">
+                <WidgetSmall />
+                <WidgetLarge />
+            </div>
         </div>
     )
 }
