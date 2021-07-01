e072d525f0ec3c9411a0ee8594017c8e97d5cca6
helter daniel <helterdaniel@hotmail.com>
2021-07-01 16:33:55 -0300

    exemplos de usuarios

--- src/dataExample.js
+++ src/dataExample.js
@@ -1,63 +1,100 @@
 export const userData = [
-        {
-            name: 'Jan',
-            "Active User": 5000,
+    {
+        name: 'Jan',
+        "Active User": 5000,
 
-        },
-        {
-            name: 'Fev',
-            "Active User": 1000,
+    },
+    {
+        name: 'Fev',
+        "Active User": 1000,
 
-        },
-        {
-            name: 'Mar',
-            "Active User": 2000,
+    },
+    {
+        name: 'Mar',
+        "Active User": 2000,
 
-        },
-        {
-            name: 'Abr',
-            "Active User": 9000,
+    },
+    {
+        name: 'Abr',
+        "Active User": 9000,
 
-        },
-        {
-            name: 'Mai',
-            "Active User": 8000,
+    },
+    {
+        name: 'Mai',
+        "Active User": 8000,
 
-        },
-        {
-            name: 'Jun',
-            "Active User": 7000,
+    },
+    {
+        name: 'Jun',
+        "Active User": 7000,
 
-        },
-        {
-            name: 'Jul',
-            "Active User": 6000,
+    },
+    {
+        name: 'Jul',
+        "Active User": 6000,
 
-        },
-        {
-            name: 'Ago',
-            "Active User": 5000,
+    },
+    {
+        name: 'Ago',
+        "Active User": 5000,
 
-        },
-        {
-            name: 'Set',
-            "Active User": 4000,
+    },
+    {
+        name: 'Set',
+        "Active User": 4000,
 
-        },
-        {
-            name: 'Out',
-            "Active User": 3000,
+    },
+    {
+        name: 'Out',
+        "Active User": 3000,
 
-        },
-        {
-            name: 'Nov',
-            "Active User": 2000,
+    },
+    {
+        name: 'Nov',
+        "Active User": 2000,
 
-        },
-        {
-            name: 'Des',
-            "Active User": 1000,
+    },
+    {
+        name: 'Des',
+        "Active User": 1000,
 
-        },
+    },
 
-    ];
+];
+
+export const userRows = [
+    {
+        id: 1,
+        username: 'Jon Snow',
+        avatar: 'https://randomuser.me/api/portraits/women/24.jpg',
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "R$120.00"
+    },
+    {
+        id: 2,
+        username: 'Jon Snow',
+        avatar: 'https://randomuser.me/api/portraits/women/24.jpg',
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "R$120.00"
+    },
+    {
+        id: 3,
+        username: 'Jon Snow',
+        avatar: 'https://randomuser.me/api/portraits/women/24.jpg',
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "R$120.00"
+    },
+    {
+        id: 4,
+        username: 'Jon Snow',
+        avatar: 'https://randomuser.me/api/portraits/women/24.jpg',
+        email: "jon@gmail.com",
+        status: "active",
+        transaction: "R$120.00"
+    },
+
+
+]
