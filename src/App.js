5cfa3108dcb3b37fdc7acfc6489b15694b71d55a
helter daniel <helterdaniel@hotmail.com>
2021-07-01 16:34:16 -0300

    userList e Router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import Sidebar from "./components/sidebar/Sidebar"
 import Topbar from "./components/topbar/Topbar"
 import "./app.css"
 import Home from "./pages/home/Home"
+import UserList from "./pages/userList/UserList"
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom"
+
 function App() {
   return (
-    <div className="App">
-      <Topbar/>
+    <Router className="App">
+      <Topbar />
       <div className="container">
-        <Sidebar/>
-        <Home/>
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+
+          </Route>
+          <Route path="/users">
+            <UserList/>
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
