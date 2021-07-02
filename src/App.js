c2e1f4fbbe891497e717a0922e02fb1aecf64b15
helter daniel <helterdaniel@hotmail.com>
2021-07-02 18:15:10 -0300

    pagina de usuario e sort import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom"
 
+import "./app.css"
 
 import Sidebar from "./components/sidebar/Sidebar"
 import Topbar from "./components/topbar/Topbar"
-import "./app.css"
+
 import Home from "./pages/home/Home"
 import UserList from "./pages/userList/UserList"
-import { BrowserRouter as Router, Switch, Route, } from "react-router-dom"
+import User from "./pages/user/User"
 
 function App() {
   return (
@@ -20,6 +22,9 @@ function App() {
           </Route>
           <Route path="/users">
             <UserList/>
+          </Route>
+          <Route path="/user/:userId">
+            <User/>
           </Route>
         </Switch>
       </div>
