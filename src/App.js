f196d553f3376384caa7f9a2a105f46c3a5512a5
helter daniel <helterdaniel@hotmail.com>
2021-07-08 21:06:07 -0300

    new product, home link e finish

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 function App() {
   return (
     <Router className="App">
@@ -36,9 +37,9 @@ function App() {
           <Route path="/product/:productId">
             <Product />
           </Route>
-          {/* <Route path="/newProduct">
+          <Route path="/newProduct">
             <NewProduct />
-          </Route> */}
+          </Route>
         </Switch>
       </div>
     </Router>
